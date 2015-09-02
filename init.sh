8acb367021227926677ff2c5f0cb0b67f42e2a40
Robert Quattlebaum <darco@deepdarc.com>
2015-09-02 17:29:54 +0000

    More updates

--- init.sh
+++ init.sh
@@ -14,7 +14,10 @@ fi
 # ----------------------------------------------------------------------
 
 mkdir /etc/postfix/tmp
-awk < /etc/mail-config/aliases '{ print $2 }' > /etc/postfix/tmp/virtual-receivers
+(
+	awk < /etc/mail-config/aliases '{ print $2 }'
+	cut -d ':' -f 1 < /etc/mail-config/passwd
+) | sort -u > /etc/postfix/tmp/virtual-receivers
 sed -r 's,(.+)@(.+),\2/\1/,' /etc/postfix/tmp/virtual-receivers > /etc/postfix/tmp/virtual-receiver-folders
 paste /etc/postfix/tmp/virtual-receivers /etc/postfix/tmp/virtual-receiver-folders > /etc/postfix/virtual-mailbox-maps
 postmap /etc/mail-config/aliases
