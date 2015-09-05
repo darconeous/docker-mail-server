17c60d62c9250948c080f4077e92c41f7202b161
Robert Quattlebaum <darco@deepdarc.com>
2015-09-05 19:22:06 +0000

    more updates

--- boot.d/dovecot.sh
+++ boot.d/dovecot.sh
@@ -3,3 +3,12 @@
 cp ${MAIL_CONFIG_DIR}/passwd /etc/dovecot/passwd
 [ "${DEBUG-0}" = "0" ] && rm -f /etc/dovecot/conf.d/10-logging.conf
 
+MAILDIR=/var/vmail
+
+for address in `cut -d ':' -f 1 < ${MAIL_CONFIG_DIR}/passwd`
+do
+	username=`echo "${address}" | sed -r 's,(.+)@(.+),\1,'`
+	hostname=`echo "${address}" | sed -r 's,(.+)@(.+),\2,'`
+	mkdir -p "${MAILDIR}/${hostname}/${username}"
+	[ -f "${MAILDIR}/${hostname}/${username}/.dovecot.sieve" ] || cp "/etc/dovecot/dovecot.sieve" "${MAILDIR}/${hostname}/${username}/.dovecot.sieve"
+done
